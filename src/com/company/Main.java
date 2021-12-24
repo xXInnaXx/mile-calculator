28314c63e11c327d3fd0d0c6b8621beaea160b0d
innakhabirova <iikhabi@mail.ru>
2021-12-24 22:42:16 +0300

    mile calculator was created

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int ticketPrice = 100;
+        int milePrice=20;
+        int totalMile=ticketPrice/milePrice;
+        System.out.println("количество миль="+totalMile);
+
+    }
+}
