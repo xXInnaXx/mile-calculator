27fd3e3735bef46ee5a02faae37240d8f62556ab
innakhabirova <iikhabi@mail.ru>
2022-11-13 23:39:10 +0300

    reformat code  was created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         int ticketPrice = 100;
-        int milePrice=20;
-        int totalMile=ticketPrice/milePrice;
-        System.out.println("количество миль="+totalMile);
+        int milePrice = 20;
+        int totalMile = ticketPrice / milePrice;
+        System.out.println("количество миль=" + totalMile);
 
     }
 }
